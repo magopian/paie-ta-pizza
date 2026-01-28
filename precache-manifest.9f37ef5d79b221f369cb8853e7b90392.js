0576b397b2dccb963baa98ff3701c4b9c9687b95
Mathieu Agopian <mathieu@agopian.info>
2026-01-28 17:42:10 +0100

    Updates

---
+++ precache-manifest.9f37ef5d79b221f369cb8853e7b90392.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a14aa3260136f2f8ee24d436e0c8430",
+    "url": "./.DS_Store"
+  },
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "./favicon.ico"
+  },
+  {
+    "revision": "ad35d8e9fe6b63d58f481d80ce5d4ad1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "./manifest.json"
+  },
+  {
+    "revision": "94cbb1d0170a390b0ed84f9ebad8a1b6",
+    "url": "./pico.min.css"
+  },
+  {
+    "revision": "0f851f437108fbe003f6",
+    "url": "./static/js/main.0f851f43.chunk.js"
+  },
+  {
+    "revision": "d5752d2fdce3c7e150f5",
+    "url": "./static/js/runtime~main.d5752d2f.js"
+  },
+  {
+    "revision": "604c6712e7dcf2851e2c",
+    "url": "./static/js/vendors~main.604c6712.chunk.js"
+  },
+  {
+    "revision": "f50bc94d71ba2a2ec6ff99219a44ed71",
+    "url": "./styles.css"
+  }
+]);
